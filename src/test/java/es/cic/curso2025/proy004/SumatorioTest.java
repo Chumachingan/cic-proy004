c89ab76b9191218dfb408fb1269461207e445845
IvanCarrera <ivanvilac@gmail.com>
2025-07-11 12:22:46 +0200

    Ciclos y arrays

---
+++ src/test/java/es/cic/curso2025/proy004/SumatorioTest.java
@@ -0,0 +1,13 @@
+package es.cic.curso2025.proy004;
+
+import org.junit.jupiter.api.Test;
+
+public class SumatorioTest {
+    @Test
+    void testLanzar() {
+
+        Sumatorio sumatorio = new Sumatorio();
+        sumatorio.lanzar();
+        
+    }
+}
